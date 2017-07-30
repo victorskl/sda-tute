c38eb6e4e9f62328ec19cf68a3e649fb777e7d08
Victor San Kho Lin <victorlin21@gmail.com>
2017-07-31 03:11:49 +1000

    Minor: added Hello.java to get src directory into git.

---
+++ SimpleWebProject/src/swen90007/Hello.java
@@ -0,0 +1,12 @@
+package swen90007;
+
+/**
+ * Just a placeholder so that src folder get into git. 
+ *
+ */
+public class Hello {
+    
+    public Hello() {
+	System.out.println("Hello");
+    }
+}
